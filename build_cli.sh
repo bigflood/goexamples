0b122d8df82ed7b575f2378a39436c714842e21a
flood <pyotaesu@gmail.com>
2017-07-31 01:19:00 +0900

    go version 1.8.3

--- build_cli.sh
+++ build_cli.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# go build -o clirun github.com/bigflood/goexamples/cli
-# -> "[?]   ???? killed   ./clirun"
-# https://github.com/golang/go/issues/19734
-
-go build -ldflags -s -o clirun github.com/bigflood/goexamples/cli
-
-
-# go build  -ldflags "-linkmode external -extldflags -static"  -o clirun github.com/bigflood/goexamples/cli
+go build -o clirun github.com/bigflood/goexamples/cli
